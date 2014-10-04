894be8bcb5d8c10523e042e47cd61665001b90dc
Lee <lvturner@me.com>
2014-10-04 11:54:04 +0800

    More watch face size tweaking

--- src/mainface.c
+++ src/mainface.c
@@ -23,19 +23,19 @@ static void initialise_ui(void) {
   s_res_bitham_30_black = fonts_get_system_font(FONT_KEY_BITHAM_30_BLACK);
   s_res_aqi_moderate = gbitmap_create_with_resource(RESOURCE_ID_AQI_MODERATE);
   // time_layer
-  time_layer = text_layer_create(GRect(0, 0, 144, 48));
+  time_layer = text_layer_create(GRect(0, 0, 144, 47));
   text_layer_set_text(time_layer, "14:09");
   text_layer_set_text_alignment(time_layer, GTextAlignmentRight);
   text_layer_set_font(time_layer, s_res_roboto_bold_subset_49);
   layer_add_child(window_get_root_layer(s_window), (Layer *)time_layer);
   
   // date_layer
-  date_layer = text_layer_create(GRect(0, -1, 54, 14));
+  date_layer = text_layer_create(GRect(0, -1, 144, 14));
   text_layer_set_text(date_layer, "Fri 03 Oct");
   layer_add_child(window_get_root_layer(s_window), (Layer *)date_layer);
   
   // warning_layer
-  warning_layer = text_layer_create(GRect(6, 50, 135, 77));
+  warning_layer = text_layer_create(GRect(6, 53, 135, 75));
   text_layer_set_background_color(warning_layer, GColorClear);
   text_layer_set_text_color(warning_layer, GColorWhite);
   text_layer_set_text(warning_layer, "Thunderstorm Warning was issued at 23:30 HKT (3 Oct 2014) ");
